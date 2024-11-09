9370aa305db5293897e361224bcd600c72c2f794
wenya <[wenya.com]>
2024-11-09 18:22:31 +0800

    添加拦截器。

--- manager/src/main/java/com/wenya/quality/ManagerApplication.java
+++ manager/src/main/java/com/wenya/quality/ManagerApplication.java
@@ -1,14 +1,17 @@
 package com.wenya.quality;
 
 import com.wenya.quality.log.annotation.EnableSysLogAspect;
+import com.wenya.quality.properties.UserAuthProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @EnableSysLogAspect
 @MapperScan("com.wenya.quality")
+@EnableConfigurationProperties(value = {UserAuthProperties.class})
 @Slf4j
 public class ManagerApplication {
 
