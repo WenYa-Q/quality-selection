7319ceb4adbb6311c9842c412f3fe6af580bddb2
wenya <1296426842@qq.com>
2024-11-27 13:26:27 +0800

    代码格式：规范格式化代码

--- manager/src/main/java/com/wenya/quality/properties/UserAuthProperties.java
+++ manager/src/main/java/com/wenya/quality/properties/UserAuthProperties.java
@@ -5,6 +5,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 
+/**
+ * 用户安全属性
+ * Description：
+ *
+ * @author wuqiulin
+ */
 @Data
 @ConfigurationProperties(prefix = "quality-selection.auth")
 public class UserAuthProperties {
