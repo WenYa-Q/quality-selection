e50cb6bedf4b40c628d7c5ba7747f239d16f6806
wenya <1296426842@qq.com>
2024-12-31 16:30:22 +0800

    新增功能：分类品牌页面查询功能

---
+++ manager/src/main/java/com/wenya/quality/service/ICategoryBrandService.java
@@ -0,0 +1,23 @@
+package com.wenya.quality.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.wenya.quality.doamin.product.CategoryBrand;
+import com.wenya.quality.dto.product.CategoryBrandDto;
+
+import java.util.List;
+
+/**
+ * 分类品牌服务
+ * Description：
+ *
+ * @author wuqiulin
+ */
+public interface ICategoryBrandService extends IService<CategoryBrand> {
+    /**
+     * 按页面查找
+     *
+     * @param categoryBrandDto 品类品牌dto
+     * @return {@link List }<{@link ? }>
+     */
+    List<CategoryBrand> findByPage(CategoryBrandDto categoryBrandDto);
+}
