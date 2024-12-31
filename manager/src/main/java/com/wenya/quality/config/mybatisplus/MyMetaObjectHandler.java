5c88b6be2a4cbd54d671c065b177f5dcaa743c2d
wenya <1296426842@qq.com>
2024-12-31 14:53:15 +0800

    其他：添加公共字段自动填充

---
+++ manager/src/main/java/com/wenya/quality/config/mybatisplus/MyMetaObjectHandler.java
@@ -0,0 +1,27 @@
+package com.wenya.quality.config.mybatisplus;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * 自动填充字段处理程序
+ * Description：
+ *
+ * @author wuqiulin
+ */
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", Date.class, new Date());
+        this.strictInsertFill(metaObject, "isDeleted", Integer.class, 0);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", Date.class, new Date());
+    }
+}
