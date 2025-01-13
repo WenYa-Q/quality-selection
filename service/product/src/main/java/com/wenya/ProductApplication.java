e806a1e7615c8811117e9b4592a25368fc995038
wenya <1296426842@qq.com>
2025-01-13 10:48:26 +0800

    新增功能：新建客户端后台服务模块

---
+++ service/product/src/main/java/com/wenya/ProductApplication.java
@@ -0,0 +1,17 @@
+package com.wenya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 产品应用
+ * Description：
+ *
+ * @author wuqiulin
+ */
+@SpringBootApplication
+public class ProductApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+}
