ecd3b8aab0c1616201c40d8bafefcdc7ae2d9534
wenya <1296426842@qq.com>
2025-01-15 17:24:57 +0800

    新增功能：用户服务模块

---
+++ quality-service/service-user/src/main/java/com/wenya/ServiceUserApplication.java
@@ -0,0 +1,19 @@
+package com.wenya;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 服务用户应用程序
+ * Description：
+ *
+ * @author wuqiulin
+ */
+@SpringBootApplication
+@Slf4j
+public class ServiceUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceUserApplication.class, args);
+    }
+}
