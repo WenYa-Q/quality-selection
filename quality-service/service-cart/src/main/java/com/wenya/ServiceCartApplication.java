82150405cd7a4ffa59a42a80a3430477345a77b5
wenya <1296426842@qq.com>
2025-02-06 14:14:07 +0800

    其他：添加接口服务依赖，启用OpenFeign

--- quality-service/service-cart/src/main/java/com/wenya/ServiceCartApplication.java
+++ quality-service/service-cart/src/main/java/com/wenya/ServiceCartApplication.java
@@ -1,8 +1,10 @@
 package com.wenya;
 
+import com.wenya.quality.anno.EnableUserWebMvcConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 购物车应用程序
@@ -11,6 +13,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @author wuqiulin
  */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableFeignClients
+@EnableUserWebMvcConfiguration
 public class ServiceCartApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceCartApplication.class, args);
